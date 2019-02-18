9d3c18a14c14cf19644fdee26e9f4ff3a4c97756
MrScopes <drewmanrules23@gmail.com>
2019-02-18 01:07:45 -0500

    Help Command

--- src/modules/commands/General/help.js
+++ src/modules/commands/General/help.js
@@ -10,7 +10,10 @@ module.exports = {
 module.exports.run = (client, message) => {
 	
 	const help = new Discord.MessageEmbed()
-		.addField('Temporary Help', 'https://github.com/MrScopes/dev-support')
+		.setColor('83cbf1')
+		.addField('Commands', `\`${client.config.prefix}help\`: bot information\n\`${client.config.prefix}ping\`: ping the bot\n\`${client.config.prefix}rules\`: view the server rules\n\n\`${client.config.prefix}eval\`: evaluate javascript\n\`${client.config.prefix}exec\`: execute a console command`, true)
+		.addField('Links', '[Discord](https://discord.io/dev-support) | [Repository](https://github.com/MrScopes/dev-support)', true)
+		.addBlankField()
 		.setFooter(`Requested by ${message.author.tag}`);
 	message.channel.send(help);
 	
